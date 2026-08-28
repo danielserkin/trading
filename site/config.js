f12830a378d4005e59a6c0af00603868975f7b19
danielserkin <daniel.serkin@gmail.com>
2026-08-28 23:01:40 +0000

    chore: configure production worker URL

--- site/config.js
+++ site/config.js
@@ -1,2 +1,2 @@
 // Optional deployment default. The user can also enter the Worker URL in Settings.
-window.TRADING_API_BASE = window.TRADING_API_BASE || "";
+window.TRADING_API_BASE = window.TRADING_API_BASE || "https://trading-control.daniel-serkin.workers.dev";
